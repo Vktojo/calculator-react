55822781ab7bd3ebc2730326361b0886a3ffe6e5
Kenzo <vktojo@hotmail.com>
2022-07-11 02:34:28 -0300

    Inlcusão dos scripts para as operações da calculadora.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,70 @@ function App() {
     const [visor, setVisor] = useState(0);
 
     function setInput(d) {
-        return;
+        var input = d.target.textContent;
+        if (input == "C") {
+            setVisor("0");
+            return;
+        }
+
+        var isCurrentOperator = isOperator(input);
+
+        // Se começar com zero, posso colocar o que vier de tela
+        if (visor == "0" && !isCurrentOperator) {
+            setVisor(input);
+            return;
+        }
+
+        // Caso mande dois operadores seguidos, prioriza o último
+        var isLastCharOperator = isOperator(visor.substr(-1));
+        if (isCurrentOperator && isLastCharOperator) {
+            // Mas se o atual for um "=", limpa. Caso contrário, substitui 
+            if (input == '=') {
+                replaceLastChar(visor, '');
+            } else {
+                setVisor(replaceLastChar(visor, input));
+            }
+            return;
+        }
+
+        // Caso seja um operador
+        if (isCurrentOperator) {
+            // se ja tem algum operador, resolve a conta
+            if (hasOperator(visor)) {
+                if (input == "=") {
+                    setVisor(eval(visor) + "");
+                } else {
+                    setVisor(eval(visor) + input);
+                }
+                return;
+            }
+            // se não tem e é "=", nem coloca
+            else if (input == "=") {
+                return;
+            }
+        }
+
+        setVisor(visor + input);
     }
+
+    function isOperator(d) {
+        var operators = ['+', '-', '*', '/', 'C', '='];
+        return operators.includes(d);
+    }
+
+    function hasOperator(d) {
+        for (let i = 0; i < d.length; i++) {
+            if (isOperator(d[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function replaceLastChar(string, charToReplace) {
+        return string.substring(0, string.length - 1) + charToReplace;
+    }
+
 
     return (
         <div className="text-center">
