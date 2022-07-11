754c593c90b59be354def76cdf5a32ea79272df2
Kenzo <vktojo@hotmail.com>
2022-07-11 02:28:54 -0300

    Adaptações no CSS

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ function App() {
 
     return (
         <div className="text-center">
-            <h1>Minha calculadora.</h1>
-            <div>
+            <h1>Minha calculadora</h1>
+            <h3>Vitor Kenzo Tojo Costa</h3>
+            <div className="center">
                 <table id="calculator">
                     <tbody>
                         <tr>
@@ -21,32 +22,32 @@ function App() {
                         <tr>
                             <td></td>
                             <td></td>
-                            <td><span onClick={setInput} id="clear">C</span></td>
-                            <td><span onClick={setInput} id="divide">/</span></td>
+                            <td><span onClick={setInput} className="operator">C</span></td>
+                            <td><span onClick={setInput} className="operator">/</span></td>
                         </tr>
                         <tr>
                             <td><span onClick={setInput}>7</span></td>
                             <td><span onClick={setInput}>8</span></td>
                             <td><span onClick={setInput}>9</span></td>
-                            <td><span onClick={setInput} id="multiply">*</span></td>
+                            <td><span onClick={setInput} className="operator">*</span></td>
                         </tr>
                         <tr>
                             <td><span onClick={setInput}>4</span></td>
                             <td><span onClick={setInput}>5</span></td>
                             <td><span onClick={setInput}>6</span></td>
-                            <td><span onClick={setInput} id="sub">-</span></td>
+                            <td><span onClick={setInput} className="operator">-</span></td>
                         </tr>
                         <tr>
                             <td><span onClick={setInput}>1</span></td>
                             <td><span onClick={setInput}>2</span></td>
                             <td><span onClick={setInput}>3</span></td>
-                            <td><span onClick={setInput} id="plus">+</span></td>
+                            <td><span onClick={setInput} className="operator">+</span></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td><span onClick={setInput}>0</span></td>
                             <td></td>
-                            <td><span onClick={setInput} id="total">=</span></td>
+                            <td><span onClick={setInput} className="operator">=</span></td>
                         </tr>
                     </tbody>
                 </table>
